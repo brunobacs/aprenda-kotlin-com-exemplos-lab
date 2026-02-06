7c965854e3ad15d25936ce343c74c90623464bd4
Bruno Bacs <brunobaacs@gmail.com>
2026-02-05 21:47:09 -0300

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,87 @@
+import java.rmi.server.UID
+import kotlin.math.max
+
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario (val nome: String, val sobrenome : String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(var nome: String, var duracao: Int, val nivel : Nivel)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
+    val nivel : Nivel
+        get() = calcularNivelNinja()
     val inscritos = mutableListOf<Usuario>()
-    
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
     }
+
+    fun calcularNivelNinja(): Nivel {
+        return conteudos.groupBy { it.nivel }
+            .maxByOrNull { it.value.size }?.key ?: Nivel.BASICO
+    }
+
+    fun definirNivel() : Nivel {
+        var basico = 0
+        var intermediario =0
+        var dificil = 0
+
+        for (conteudo in conteudos){
+            when (conteudo.nivel) {
+                Nivel.BASICO -> basico ++
+                Nivel.INTERMEDIARIO -> intermediario ++
+                Nivel.DIFICIL -> dificil ++
+            }
+        }
+
+        return when {
+            dificil >= intermediario && dificil >= basico -> Nivel.DIFICIL
+            intermediario >= basico -> Nivel.INTERMEDIARIO
+            else -> Nivel.BASICO
+        }
+
+    }
+    
+    fun getNivel () : Nivel{
+        return nivel
+    }
+
+    fun getInscritos () : List<Usuario> {
+        return inscritos
+    }
+
+
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    
+
+    val usuarioA = Usuario("Joao", "A")
+    val usuarioB = Usuario("Jose", "B")
+    val usuarioC = Usuario("Pedro", "C")
+
+    val conteudo1 = ConteudoEducacional("Intro a Java", 60, Nivel.BASICO)
+    val conteudo2 = ConteudoEducacional("POO", 60, Nivel.INTERMEDIARIO)
+    val conteudo3 = ConteudoEducacional("SOLID", 30, Nivel.INTERMEDIARIO)
+    val conteudo4 = ConteudoEducacional("Engenharia de Software", 120, Nivel.DIFICIL)
+
+    val conteudos = listOf(conteudo1, conteudo2, conteudo3, conteudo4)
+
+    val formacao = Formacao("Curso de Java", conteudos)
+
+    formacao.matricular(usuarioA)
+    formacao.matricular(usuarioB)
+
+    println("Formação: ${formacao.nome}")
+
+    println("Nível calculado (Ninja): ${formacao.getNivel()}")
+    println("Nível calculado (Manual): ${formacao.definirNivel()}")
+
+    println("--- Inscritos ---")
+    for (u in formacao.getInscritos()) {
+        println("Usuário: ${u.nome} ${u.sobrenome}")
+    }
+
 }
